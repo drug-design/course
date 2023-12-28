489e1b9de7676a1c143e65ed118e15e99a2eb2ea
Elie Cohen <eliecohen1@gmail.com>
2023-12-28 18:01:57 +0200

    complete the peptidometics 3d Molecules

--- docs/molecules/peptidomimetics/a72517.js
+++ docs/molecules/peptidomimetics/a72517.js
@@ -87,38 +87,3 @@ Promise.all(moleculeDetails.map(loadMolecule)).then(function (loadedMolecules) {
       }
   });
 });
-
-
-
-
-
-// Promise.all([
-//   stage.loadFile("./../../molecules/peptidomimetics/",{name: 'renin-red'}).then(function (o) {
-//     o.addRepresentation("backbone", {color: "red"})
-//     o.addRepresentation("licorice", {color: 'pink', sele: 'TYR and 75'})
-//     return o
-//   }),
-
-//   stage.loadFile("./../../molecules/molecular-similarity/receptor-conformational-change-renin-2.pdb",{name: 'renin-blue'}).then(function (o) {
-//     o.addRepresentation("backbone", {color: "blue"})
-//     o.addRepresentation("licorice", {color: 'skyblue', sele: 'TYR and 83'})
-//     return o
-//   }),
-
-//   stage.loadFile("./../../molecules/molecular-similarity/receptor-conformational-change-renin-ligand.sd",{name: 'lig'}).then(function (o) {
-//     o.addRepresentation("licorice", {color: "yellow"})
-//     return o
-//   })
-// ]).then(function (ol) {
-//   stage.autoView();
-//   stage.viewerControls.zoom(0.4);
-
-//   ol.forEach(function (loadedMolecule, index) {
-//     var control = moleculeControls[index];
-//     if (control) {
-//       molecules[control.id] = loadedMolecule;
-//       loadedMolecule.setVisibility(control.checked);
-//     }
-//   });
-
-// });
--- docs/site/changelog.md
+++ docs/site/changelog.md
@@ -4,5 +4,5 @@
 Launch of drugdesign.org the new "drug design" and "drug discovery" sharing platform. The current version includes all "Molecular Conceptor"  content developed by Synergix Ltd between 2001-2013.
 
 ### December 20, 2023
-I have begun working on rendering molecules in 3D and have chosen the NGL Viewer [nglviewer.org](https://nglviewer.org/) as a powerful is a web-based application for molecular visualization. My first chapter, featuring several 3D molecules, is already complete. see [Molecular Similarity](chapters/molecular-similarity.md)
+I have begun working on rendering molecules in 3D and have chosen the NGL Viewer [nglviewer.org](https://nglviewer.org/) as a powerful is a web-based application for molecular visualization. My first chapter, featuring several 3D molecules, is already complete. see [Molecular Similarity](https://www.drugdesign.org/chapters/molecular-similarity/)
 
